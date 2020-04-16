36f45cd4769661a6f213fd4e9916e3e567220116
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-04-16 22:58:12 +0700

    fixing port for db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controller/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-rigid-77377",
     user: "postgres",
     password: "123456",
     database: "test",
